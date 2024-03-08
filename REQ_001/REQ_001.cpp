e4488ed4d27fac8908db31236c48c8d685af9d85
Paul Santiago <70616143+prsanti@users.noreply.github.com>
2024-03-08 14:08:58 -0500

    create test files

---
+++ REQ_001/REQ_001.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace REQ001
+{
+	TEST_CLASS(REQ001)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
